4712565027cc33d4fc65ce42800000347086b99e
Leticia <leticiards.04@gmail.com>
2022-01-06 10:38:37 -0300

    Adicionando parte final do código que funciona, professor n responde o curso

--- Backend/src/routes/TaskRoutes.js
+++ Backend/src/routes/TaskRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const TaskController = require('../controller/TaskController');
 
-router.post('/', TaskController.create);
+const TaskController = require('../controller/TaskController');
+const TaskValidation = require('../middlewares/TaskValidation');
+
+router.post('/', TaskValidation, TaskController.create);
+router.put('/:id', TaskController.update);
+router.get('/filter/all', TaskController.all);
 
 module.exports = router;
